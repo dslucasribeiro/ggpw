1350e99e8fc8cb41a0775e91e441accfd4a5bf96
LucsRibeiro <lucas.ribeiro@simcfc.com.br>
2025-01-31 15:49:15 -0300

    env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iqonxznqsocuayvrcmdg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxb254em5xc29jdWF5dnJjbWRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NDgzNjgsImV4cCI6MjA1MzQyNDM2OH0.LsYdvmm-swAU13KJvWIe9kkFkE_69gREqlbElkmQiQY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
